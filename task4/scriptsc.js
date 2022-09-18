114d8a4557e2f401036b8f7898d92cbc558a1b2f
Mananya Ugadhi <107743713+Mananya07@users.noreply.github.com>
2022-09-18 20:33:36 +0530

    Add files via upload

---
+++ task4/scriptsc.js
@@ -0,0 +1,19 @@
+function display(val){
+    document.getElementById('result').value += val
+    return val
+}
+
+function solve(){
+    let x = document.getElementById('result').value
+    let y = eval(x);
+    document.getElementById('result').value = y
+    return y
+}
+
+function clearScreen(){
+    document.getElementById("result").value = "";
+}
+
+function backspace(){
+    document.getElementById("result").value = document.getElementById("result").value.slice(0, -1);
+}
